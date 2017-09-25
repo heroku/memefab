dce8b6e00aa95b537436cb64f9f7b9b60084abd5
Josh W Lewis <josh.w.lewis@gmail.com>
2017-09-25 08:20:36 -0500

    Extract transformation logic

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,28 @@
 module ImagesHelper
+  def meme_transformation_url(upload_id, top_text, bottom_text)
+    transforms = [{ width: 600, crop: :scale }]
+    transforms << meme_text_overlay(top_text, :north) if top_text.present?
+    transforms << meme_text_overlay(bottom_text, :south) if bottom_text.present?
+    Cloudinary::Utils.cloudinary_url(upload_id, transformation: transforms)
+  end
+
+  private
+
+  def meme_text_overlay(text, gravity)
+    {
+      overlay: {
+        text: text.upcase,
+        font_family: 'Impact',
+        font_size: 100,
+        font_weight: :bold,
+        stroke: :stroke,
+      },
+      width: 580,
+      height: 100,
+      gravity: gravity,
+      color: "#FFF",
+      crop: :scale,
+      border: "8px_solid_black"
+    }
+  end
 end
--- app/mediators/meme_creator.rb
+++ app/mediators/meme_creator.rb
@@ -1,4 +1,5 @@
 class ImageCreator < Mediator
+  include ImagesHelper
 
   def initialize(top:, bottom:, image:, model: Meme, uploader: Cloudinary::Uploader)
     @top      = top
@@ -30,62 +31,18 @@ class ImageCreator < Mediator
   end
 
   def transform_url
-    Cloudinary::Utils.cloudinary_url(image.upload_id,
-      transformation: [
-        {
-          width: 600,
-          crop: :scale
-        },
-        {
-          overlay: {
-            text: top.upcase,
-            font_family: 'Impact',
-            font_size: 100,
-            font_weight: :bold,
-            stroke: :stroke,
-          },
-          width: 580,
-          height: 100,
-          gravity: :north,
-          color: "#FFF",
-          crop: :scale,
-          border: "8px_solid_black"
-        },
-        {
-          overlay: {
-            text: bottom.upcase,
-            font_family: 'Impact',
-            font_size: 100,
-            font_weight: :bold,
-            stroke: :stroke,
-          },
-          width: 580,
-          height: 100,
-          gravity: :south,
-          color: "#FFF",
-          crop: :scale,
-          border: "8px_solid_black"
-        }
-      ]
-    )
+    @transform_url = meme_transformation_url(image.upload_id, top, bottom)
   end
 
   def upload
-    uploader.upload(transform_url)
-  end
-
-  def upload
-    uploader.upload(path,
-      allowed_formats: "png,jpg,gif",
-      public_id: upload_id
-    )
+    uploader.upload(transform_url, public_id: upload_id)
   end
 
   def create_record
     model.create!(
-      top: top,
-      bottom: bottom,
-      image: image,
+      top:       top,
+      bottom:    bottom,
+      image:     image,
       upload_id: upload_id
     )
   end
